95a09218ac4998a176a964a9f98b7c153a39fc0f
Aaron Claydon <claydon.aaron@gmail.com>
2025-03-28 00:21:03 +0000

    batch

--- aks.tf
+++ aks.tf
@@ -2,12 +2,50 @@ locals {
   nodes = {
     "workers" = {
       name            = "workers"
-      vm_size         = "Standard_D4as_v5"
+      vm_size         = "Standard_D8as_v5"
       node_count      = 1
+      priority        = "Spot"
+      eviction_policy = "Delete"
+      os_disk_size_gb = 48
+      vnet_subnet_id  = azurerm_subnet.kube.id
+    },
+    "medium-batch" = {
+      name            = "mbatch"
+      vm_size         = "Standard_D4as_v5"
+      node_count      = 0
       priority        = "Spot"
       eviction_policy = "Delete"
       os_disk_size_gb = 32
       vnet_subnet_id  = azurerm_subnet.kube.id
+      node_labels = {
+        "kube.travigo.app/batch-burst-size" = "medium"
+      },
+      node_taints = [
+        "kube.travigo.app/batch-burst=true:NoSchedule"
+      ]
+
+      enable_auto_scaling = true
+      min_count = 0
+      max_count = 1
+    },
+    "large-batch" = {
+      name            = "lbatch"
+      vm_size         = "Standard_D8as_v5"
+      node_count      = 0
+      priority        = "Spot"
+      eviction_policy = "Delete"
+      os_disk_size_gb = 32
+      vnet_subnet_id  = azurerm_subnet.kube.id
+      node_labels = {
+        "kube.travigo.app/batch-burst-size" = "large"
+      },
+      node_taints = [
+        "kube.travigo.app/batch-burst=true:NoSchedule"
+      ]
+
+      enable_auto_scaling = true
+      min_count = 0
+      max_count = 1
     }
   }
 }
@@ -23,7 +61,7 @@ module "aks" {
   vnet_subnet_id      = azurerm_subnet.kube.id
   node_pools          = local.nodes
 
-  log_analytics_workspace_enabled   = false
+  log_analytics_workspace_enabled   = true
   rbac_aad                          = true
   rbac_aad_managed                  = true
   role_based_access_control_enabled = true
@@ -36,4 +74,9 @@ module "aks" {
   os_disk_size_gb     = 32
   only_critical_addons_enabled = true
   temporary_name_for_rotation  = "systemrotate"
+
+  // Scale
+  auto_scaler_profile_enabled = true
+  auto_scaler_profile_skip_nodes_with_system_pods = false
+  auto_scaler_profile_skip_nodes_with_local_storage = false
 }
